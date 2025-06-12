1293d5adf353472f59e03efc63a71db8122b3996
Minato0256 <153624974+Minato0256@users.noreply.github.com>
2025-06-12 05:36:27 +0000

    次はToDoタスクを表示させようから

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+let todos = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ToDo App' });
+});
+
+router.post('/', function(req, res, next) {
+  const todo = req.body.add;
+  todos.push(todo);
+  res.render('index', { title: 'ToDo App' });
 });
 
 module.exports = router;
