6f93cb95670fdd5c6ba0d841794678e4f2e04eae
Mr-T18 <125945725+Mr-T18@users.noreply.github.com>
2025-06-19 04:42:31 +0000

    登録したタスクを下部に表示した

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+let todos = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ToDo App' });
+  res.render('index', { 
+    title: 'ToDo App',
+    todos: todos,
+   });
 });
 
 router.post("/", function(req,res,next) {
-  res.render("index", {title: "ToDo App"});
+  const todo = req.body.add;
+  todos.push(todo);
+  res.redirect("/");
 })
 
 module.exports = router;
